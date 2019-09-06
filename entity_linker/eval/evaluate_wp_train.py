c2dfb8a9b64846ba0bb7b5c15bd4565da1ccaeda
svlandeg <sofie.vanlandeghem@gmail.com>
2019-09-06 10:17:45 +0200

    restricting annotation to dev articles

--- entity_linker/eval/evaluate_wp_train.py
+++ entity_linker/eval/evaluate_wp_train.py
@@ -11,13 +11,17 @@ from prodigy.util import write_jsonl
 Basically this is to assess how correct the automatically generated data is. """
 
 kb_dir = "C:/Users/Sofie/Documents/data/spacy_test_CLI_KB/"
-training_dir = Path("C:/Users/Sofie/Documents/data/spacy_eval_train/")
-output_file = Path("./training_100.jsonl")
+training_dir = Path("C:/Users/Sofie/Documents/data/spacy_test_CLI_train_dataset/")
+output_file = Path("./dev_training.jsonl")
 
 annotations_file = Path("./annotations_eval-wp-3_5sept2019.jsonl")
 
 
-def write():
+def is_dev(article_id):
+    return article_id.endswith("3")
+
+
+def write(limit=None):
     nlp = spacy.load(Path(kb_dir) / "nlp")
 
     cnt_articles = 0
@@ -25,70 +29,80 @@ def write():
     text_dicts = []
     for textfile in training_dir.iterdir():
         if textfile.name.endswith(".txt"):
-            cnt_articles += 1
             article_id = textfile.name.split(".")[0]
-            text = None
-            with textfile.open("r", encoding="utf8") as f:
-                try:
-                    text = f.read()
-                except Exception as e:
-                    print("Problem parsing article", article_id, e)
+            if is_dev(article_id):
+                if not limit or cnt_articles < limit:
+                    cnt_articles += 1
+                    text = None
+                    with textfile.open("r", encoding="utf8") as f:
+                        try:
+                            text = f.read()
+                        except Exception as e:
+                            print("Problem parsing article", article_id, e)
 
-            doc = nlp(text)
+                    doc = nlp(text)
 
-            gold_file_loc = article_id + "_gold.csv"
-            entityfile_loc = training_dir / gold_file_loc
-            with entityfile_loc.open("r", encoding="utf8") as entityfile:
-                for line in entityfile:
-                    fields = line.replace("\n", "").split(sep="|")
-                    article_id = fields[0]
-                    alias = fields[1]
-                    wd_id = fields[2]
-                    if article_id != "article_id":
-                        entity_start = int(fields[3])
-                        entity_end = int(fields[4])
+                    gold_file_loc = article_id + "_gold.csv"
+                    entityfile_loc = training_dir / gold_file_loc
+                    with entityfile_loc.open("r", encoding="utf8") as entityfile:
+                        for line in entityfile:
+                            fields = line.replace("\n", "").split(sep="|")
+                            article_id = fields[0]
+                            alias = fields[1]
+                            wd_id = fields[2]
+                            if article_id != "article_id":
+                                entity_start = int(fields[3])
+                                entity_end = int(fields[4])
 
-                        # using sentence boundaries (or default to adding 200 characters)
-                        text_start = max(0, entity_start-100)
-                        text_end = min(len(text), entity_end+100)
+                                # using sentence boundaries (or default to adding 200 characters)
+                                text_start = max(0, entity_start-100)
+                                text_end = min(len(text), entity_end+100)
 
-                        for sent in doc.sents:
-                            if sent.start_char <= entity_start and sent.end_char >= entity_end:
-                                text_start = sent.start_char
-                                text_end = sent.end_char
+                                for sent in doc.sents:
+                                    if sent.start_char <= entity_start and sent.end_char >= entity_end:
+                                        text_start = sent.start_char
+                                        text_end = sent.end_char
 
-                        # change the offsets to the sentence
-                        span_dict = {
-                            "start": entity_start - text_start,
-                            "end": entity_end - text_start,
-                            "parsed_WP_ID": wd_id,
-                        }
+                                # change the offsets to the sentence
+                                span_dict = {
+                                    "start": entity_start - text_start,
+                                    "end": entity_end - text_start,
+                                    "parsed_WP_ID": wd_id,
+                                }
 
-                        text_dicts.append({"article_id": article_id,
-                                           "text": text[text_start:text_end],
-                                           "spans": [span_dict],
-                                           "sent_offset": text_start})
-                        cnt_entities += 1
+                                text_dicts.append({"article_id": article_id,
+                                                   "text": text[text_start:text_end],
+                                                   "spans": [span_dict],
+                                                   "sent_offset": text_start})
+                                cnt_entities += 1
     write_jsonl(output_file, text_dicts)
-    print("Analysed", cnt_articles, "articles")
+    print("Analysed", cnt_articles, "dev articles")
     print("Found", cnt_entities, "entities")
 
 
 def read():
     with output_file.open("r", encoding="utf8") as json_file:
         json_list = list(json_file)
-        print("read", len(json_list), "lines")
+        print("Read", len(json_list), "lines")
+
+    articles = set()
+    mentions = 0
 
     for json_str in json_list:
-        print(json_str)
+        # print(json_str)
         result = json.loads(json_str)
+        article_id = result["article_id"]
+        articles.add(article_id)
         text = result["text"]
         for span in result["spans"]:
             parsed_wp = span["parsed_WP_ID"]
             start_char = int(span["start"])
             end_char = int(span["end"])
             mention = text[start_char:end_char]
-            print("found mention", mention, "parsed", parsed_wp)
+            # print("found mention", mention, "parsed", parsed_wp)
+            mentions += 1
+
+    print("Total:", mentions, "annotations in", len(articles), "articles")
 
 
 def analyse():
@@ -137,12 +151,12 @@ def analyse():
 
 if __name__ == "__main__":
     # STEP 0: first write the JSONL from the original training files
-    # write()
+    write(limit=1000)
 
     # STEP 1: read the created JSONL to be sure it's OK
-    # read()
+    read()
 
     # STEP 2: run the actual annotations with the Prodigy recipe "entity_linker.eval"
 
     # STEP 3: now run the stats on the manual annotations
-    analyse()
+    # analyse()
